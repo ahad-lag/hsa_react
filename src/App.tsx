e1a3be395464010b680ca45f859ac8c5298a00dd
Ahad Lag <mr.ahad.lag@gmail.com>
2022-12-24 18:16:50 +0330

    reformat codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes , Route } from 'react-router-dom';
 import MasterPage from './components/masterPage';
 import NotFound from './router/notFound';
-import UserPage from './router/user';
+import UserIndex from './router/user';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/user" element={<UserPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <MasterPage>
+      <Routes>
+        <Route path="/user" element={<UserIndex />} />
+        <Route path='/user/insert' element={<div>ahad</div>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </MasterPage>
   );
 }
 
