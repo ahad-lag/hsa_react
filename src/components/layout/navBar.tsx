ccadbe77fef7a515fe215b719c6924e6df65557e
Ahad Lag <mr.ahad.lag@gmail.com>
2023-02-07 15:00:02 +0330

    complete logout modal for exit button

--- src/components/layout/navBar.tsx
+++ src/components/layout/navBar.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import AlertToast from '../global/alertToast';
+import LogoutModal from '../modal/logoutModal';
 
 interface Props {
   setSideBar : React.Dispatch<React.SetStateAction<boolean>>
@@ -14,6 +15,7 @@ const NavBar : React.FC<Props> = ({ setSideBar }) => {
   const cookies = new Cookies();
   const navigate = useNavigate();
   const { token , userInfo } = useAuth();
+  const [ logoutModal , setLogoutModal ] = React.useState(false);
   
   const logout = () => {
     let res = axios.post('http://127.0.0.1:8000/api/logout',{},{
@@ -51,11 +53,12 @@ const NavBar : React.FC<Props> = ({ setSideBar }) => {
             <span className="flex bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded border border-gray-500">{userInfo?.first_name && `${userInfo?.first_name} ${userInfo?.last_name}`}</span>
           </div>
           <div className="pl-3">
-            <svg onClick={logout} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-rose-700 cursor-pointer">
+            <svg onClick={() => setLogoutModal(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-rose-700 cursor-pointer">
               <path strokeLinecap="round" strokeLinejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
             </svg>
           </div>
         </div>
+        { logoutModal && <LogoutModal msg={`${userInfo.first_name} میخوای بری بیرون؟؟!؟`} setShowModal={setLogoutModal} showModal={logoutModal} logout={logout}/> }
       </div>
   )
 }
